c5b490dcd25920678e67354cab3dee6a5d563b03
NgocPhat <ngonngocphat@gmail.com>
2024-01-07 20:05:10 +0700

    add pool object

---
+++ MainInitialize.cs
@@ -0,0 +1,34 @@
+﻿namespace UITemplate
+{
+    using System.Collections.Generic;
+    using UITemplate.Scripts.Extension.Base;
+    using UITemplate.Scripts.Extension.ObjectPool;
+    using UnityEngine;
+    using Object = UnityEngine.Object;
+
+    public class MainInitialize : MonoBehaviour
+    {
+        private readonly List<MonoService> listServiceHadCreate = new List<MonoService>();
+        
+        private async void Awake()
+        {
+            // add service
+            this.listServiceHadCreate.Add(CreateService<ObjectPoolManager>());
+            
+            foreach (var monoService in this.listServiceHadCreate)
+            {
+                await monoService.Init();
+            }
+            
+            MonoService CreateService<T>() where T : MonoBehaviour
+            {
+                var newService = new GameObject { transform = { position = Vector3.zero, rotation = Quaternion.identity }, name = typeof(T).Name };
+                newService.AddComponent<T>();
+                Object.DontDestroyOnLoad(newService);
+                return newService.GetComponent<T>() as MonoService;
+            }
+        }
+
+        
+    }
+}
---
+++ Scripts/Extension/Base/IService.cs
@@ -0,0 +1,9 @@
+﻿namespace UITemplate.Scripts.Extension.Base
+{
+    using Cysharp.Threading.Tasks;
+
+    public interface IService
+    {
+        public virtual UniTask Init() { return UniTask.CompletedTask;}
+    }
+}

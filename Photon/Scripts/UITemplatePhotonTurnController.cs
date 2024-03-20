432c9a5d33e680ba3e081b91d0378f644bb97b4d
PhatNN <107571396+ngocphat03@users.noreply.github.com>
2024-03-20 22:51:16 +0700

    Revert "update turn signal"
    This reverts commit a9d05fb5ef114148c6aa97a6bfab1537774720c9.

--- Photon/Scripts/UITemplatePhotonTurnController.cs
+++ Photon/Scripts/UITemplatePhotonTurnController.cs
@@ -4,35 +4,44 @@ namespace UITemplate.Photon.Scripts
 {
     using System.Collections.Generic;
     using System.Linq;
+    using Controller;
     using global::Photon.Pun;
     using global::Photon.Realtime;
+    using UITemplate.Photon.Signals;
+    using UnityEngine;
     using Zenject;
 
     public class UITemplatePhotonTurnController : MonoBehaviourPunCallbacks, IInitializable
     {
-        [Inject]
-        public SignalBus SignalBus;
+        [Inject] public SignalBus SignalBus;
 
         public List<Player> ListPlayerInRoom => PhotonNetwork.PlayerList.ToList();
 
-        public int ActorNumber { get; private set; }
+        public int CurrentTurn { get; private set; }
 
-        public Player PlayerCurrentTurn => this.ListPlayerInRoom.Find(player => player.ActorNumber == this.ActorNumber);
+        public Player PlayerCurrentTurn => this.ListPlayerInRoom[this.CurrentTurn];
 
-        public void Initialize() { this.gameObject.AddComponent<PhotonView>(); }
+        public void Initialize()
+        {
+            this.gameObject.AddComponent<PhotonView>();
+            this.SignalBus.Subscribe<StartOnlineGameSignal>(this.StartGame);
+        }
 
-        public void SetTurn(int indexPlayer) { this.ActorNumber = indexPlayer; }
+        private void StartGame(StartOnlineGameSignal signal) { this.SetTurn(signal.ActorTurn); }
+
+        public void SetTurn(int indexPlayer) { this.CurrentTurn = indexPlayer; }
 
         public void NextTurn()
         {
-            if (this.ActorNumber == this.ListPlayerInRoom[0].ActorNumber)
-            {
-                this.ActorNumber = this.ListPlayerInRoom[1].ActorNumber;
-            }
-            else
-            {
-                this.ActorNumber = this.ListPlayerInRoom[0].ActorNumber;
-            }
+            this.CurrentTurn = this.CurrentTurn == 0 ? 1 : 0;
+
+            this.NotifyForCurrentPlayer();
+        }
+
+        public void NotifyForCurrentPlayer()
+        {
+            Debug.Log("NotifyForCurrentPlayer");
+            this.SignalBus.Fire(new NotifyForAllPlayerSignal());
         }
     }
 }
--- Photon/Signals/ChangeTurnSignal.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UITemplate.Photon.Signals
-{
-    public class ChangeTurnSignal
-    {
-        public int ActorNumber { get; set; }
-
-        public ChangeTurnSignal(int actorNumber) { this.ActorNumber = actorNumber; }
-    }
-}

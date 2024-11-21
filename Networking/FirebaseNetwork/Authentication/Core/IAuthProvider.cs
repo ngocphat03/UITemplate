5ff15800a142ab4078f187abaf9fd8e25090e359
PhatNN <107571396+ngocphat03@users.noreply.github.com>
2024-11-22 00:42:39 +0700

    remove: networking

--- Networking/FirebaseNetwork/Authentication/Core/IAuthProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AXitUnityTemplate.Networking.FirebaseNetwork.Authentication.Core
-{
-    using System;
-
-    public interface IAuthProvider
-    {
-        public void Login(Action onSuccess, Action onFailure);
-    }
-}

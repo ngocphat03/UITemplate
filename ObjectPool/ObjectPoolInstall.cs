e73a1c83015aa19bc78d51894985fcd69e4adc40
PhatNN <107571396+ngocphat03@users.noreply.github.com>
2024-11-22 01:04:55 +0700

    update: object pool suport vcontainer and zenject

--- ObjectPool/ObjectPoolInstall.cs
+++ ObjectPool/ObjectPoolInstall.cs
@@ -1,9 +1,22 @@
 ﻿namespace AXitUnityTemplate.ObjectPool
 {
+#if ZENJECT
     using Zenject;
 
     public class ObjectPoolInstall : Installer<ObjectPoolInstall>
     {
         public override void InstallBindings() { this.Container.Bind<ObjectPoolManager>().AsCached().NonLazy(); }
     }
+
+#elif VCONTAINER
+    using VContainer;
+
+    public class ObjectPoolInstall
+    {
+        public static void Install(IContainerBuilder builder) { builder.Register<ObjectPoolManager>(Lifetime.Singleton); }
+    }
+
+#else
+    // Mono
+#endif
 }
